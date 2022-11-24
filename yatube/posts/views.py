f39ed23b85bd7ce722619c5a7f412d9049519978
Евгений Коршунов <zamter@yandex.ru>
2022-11-24 09:05:05 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+# ice_cream/views.py
+from django.http import HttpResponse
+
+
+# Главная страница
+def index(request):    
+    return HttpResponse('Главная страница')
+
+
+# Страница конкретной группы
+def group_posts(request, slug):
+    return HttpResponse(f'Страница группы {slug}')
